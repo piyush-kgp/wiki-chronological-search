b1998548a1f9a614e595e61e431540c32ccd18d9
piyush-kgp <piyushsinghkgpian@gmail.com>
2018-12-03 05:21:24 +0530

    deploy port

--- deploy.py
+++ deploy.py
@@ -20,5 +20,5 @@ def search():
         return flask.jsonify({'valid_search': False, 'search_result': search_result})
 
 # app.config['TEMPLATES_AUTO_RELOAD'] = True
-# app.run('0.0.0.0', 8000, debug = True)
-app.run()
+app.run('0.0.0.0', 8000, debug = True)
+# app.run()
